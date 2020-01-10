d02080b1f3ed1faafe839598a1f81d055c575e36
Ana Perisic <anaperisictb@yahoo.com>
2020-01-10 16:33:49 +0100

    #funkcionalnost_igre Dodano par metoda za funkcionalnost igre

--- test.py
+++ test.py
@@ -9,6 +9,20 @@ def kreiraj_tablu():
 	tabla = np.zeros((BROJ_REDOVA,BROJ_KOLONA))
 	return tabla
 
+#F-ja koja postavlja token na odgovarajucu poziciju u matrici
+def postavi_token(tabla,red,kolona,token):
+	tabla[red][kolona] = token
+
+#F-ja koja provera da li je validna lokacija, tj da li je 5 red prosledjene kolone slobodan
+def da_li_je_popunjena_kolona(tabla, kolona):
+	return tabla[BROJ_REDOVA-1][kolona] == 0
+
+#F-ja koja vraca indeks prvog slobodnog reda
+def get_sledeci_slobodan_red(tabla,kolona):
+	for i in range(BROJ_REDOVA):
+		if tabla[i][kolona] == 0:
+			return i
+
 tabla = kreiraj_tablu()
 game_over = False
 turn = 0
@@ -21,3 +35,7 @@ while not game_over:
 	# Ponudi unos drugom igracu
 	else:
 		selection = int(input("Igracu 2, unesite vasu kolonu (0-6):"))
+
+	# prelazak na sledeceg igraca, matematicki da uvek bude izmedju 0-1
+	turn += 1
+	turn = turn % 2
