e0fc64bfea2555dfeadc2323d89769e3bb47901b
Pufke <pufke1998@gmail.com>
2020-01-23 18:35:56 +0100

    #implementiran dinamicki niz i skladistenje podataka u njega

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pygame
 import sys
 import math
 import random
+import ctypes
 
 PLAYER = 0
 AI = 1
@@ -215,6 +216,61 @@ def minimax(tabla, depth, alpha, beta, maximizingPlayer):
 
 		return kolonaumn, value
 
+class DynamicArray(object): 
+    ''' 
+    DYNAMIC ARRAY CLASS (Similar to Python List) 
+    '''
+    def __init__(self): 
+        self.n = 0 # Count actual elements (Default is 0) 
+        self.capacity = 1 # Default Capacity 
+        self.A = self.make_array(self.capacity) 
+          
+    def __len__(self): 
+        """ 
+        Return number of elements sorted in array 
+        """
+        return self.n 
+      
+    def __getitem__(self, k): 
+        """ 
+        Return element at index k 
+        """
+        if not 0 <= k <self.n: 
+            # Check it k index is in bounds of array 
+            return IndexError('K is out of bounds !')  
+          
+        return self.A[k] # Retrieve from the array at index k 
+          
+    def append(self, ele): 
+        """ 
+        Add element to end of the array 
+        """
+        if self.n == self.capacity: 
+            # Double capacity if not enough room 
+            self._resize(2 * self.capacity)  
+          
+        self.A[self.n] = ele # Set self.n index to element 
+        self.n += 1
+          
+    def _resize(self, new_cap): 
+        """ 
+        Resize internal array to capacity new_cap 
+        """
+          
+        B = self.make_array(new_cap) # New bigger array 
+          
+        for k in range(self.n): # Reference all existing values 
+            B[k] = self.A[k] 
+              
+        self.A = B # Call A the new bigger array 
+        self.capacity = new_cap # Reset the capacity 
+          
+    def make_array(self, new_cap): 
+        """ 
+        Returns a new array with new_cap capacity 
+        """
+        return (new_cap * ctypes.py_object)() 
+
 tabla = kreiraj_tablu()
 game_over = False
 
@@ -233,6 +289,9 @@ pygame.display.update()
 myfont = pygame.font.SysFont("monospace", 55)
 
 turn = random.randint(PLAYER, AI)
+
+# Inijalizacija dinamickog niza
+nizPoteza = DynamicArray() 
 
 while not game_over:
 
@@ -280,7 +339,12 @@ while not game_over:
 		if da_li_je_popunjena_kolona(tabla,kolona):
 			red = get_sledeci_slobodan_red(tabla,kolona)
 			postavi_token(tabla,red,kolona,AI_TOKEN)
-
+	
+			# Appendovanje novog elemnta, tj kolone koja je odigrana  
+			nizPoteza.append(kolona) 
+			for i in range(len(nizPoteza)):
+				print("Broj poteza" ,i+1, " AI je odigrao polje ", nizPoteza[i] )
+			
 			if winning_move(tabla,AI_TOKEN):
 				label = myfont.render("POBEDA CRNOG !!", 1, CRNA)
 				screen.blit(label, (10,5))
